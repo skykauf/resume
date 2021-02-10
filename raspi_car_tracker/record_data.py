ca6825c5203d1af8cbbb3eed1dd47f1972ac1908
Skyler Kaufman <skylerkaufman@gmail.com>
2021-02-10 16:37:39 +0000

    added print debugging

--- raspi_car_tracker/record_data.py
+++ raspi_car_tracker/record_data.py
@@ -7,7 +7,9 @@ from config import *
 class DataRecorder:
     def __init__(self,max_dur=10):
         self.isStarted = False
+        self.printAll=True
         self.max_duration = max_dur
+        
         self.start_datetime = datetime.now()
         self.script_start_datedir = os.path.join(datadir,self.start_datetime.strftime("%m-%d-%Y"))
         self.script_start_datetime = self.start_datetime.strftime("%m-%d-%Y_%H:%M:%S")
@@ -28,6 +30,8 @@ class DataRecorder:
             while True:
                 try:
                     report = session.next()
+                    if self.printAll:
+                        print(report)
                     # Wait for a 'TPV' report and display the current time
                     if report['class'] == 'TPV':
                         lat = report['lat']
@@ -40,16 +44,16 @@ class DataRecorder:
                         # store data in csv file
                         f.write(str(lat)+','+str(lon)+','+str(local_timestamp)+'\n')
                         
-                        if local_timestamp-self.start_timestamp >self.max_duration:
-                            print(self.max_duration,"seconds elapsed")
-                            break
+                    if local_timestamp-self.start_timestamp >self.max_duration:
+                        print(self.max_duration,"seconds elapsed")
+                        break
                 except KeyError:
                     print("GPS not returning latitude or longitude")
                     break
                 except StopIteration:
                     session = None
                     print("GPSD has terminated")
-
+                    
     def write_gps_route_raw(self):
         map_params = {"key":openstreetmap_apikey,"bestfit":",".join([str(min(lats)-.01), str(min(longs)-.01), str(max(lats)+.01), str(max(longs)+.01)]), "size":"1920, 960", "shape":",".join(latlongs)}
         mapimage = requests.get("http://www.mapquestapi.com/staticmap/v4/getmap", params=map_params)
