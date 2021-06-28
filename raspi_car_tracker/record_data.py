7a3936345c35fec08378379cb50a1bdf03542aa3
Skyler Kaufman <skylerkaufman@gmail.com>
2021-06-28 10:32:55 -0400

    starting again

--- raspi_car_tracker/record_data.py
+++ raspi_car_tracker/record_data.py
@@ -7,17 +7,19 @@ import requests
 from PIL import Image
 from io import BytesIO
 
+# experimental
+import multiprocessing
+
 class DataRecorder:
-    def __init__(self,max_dur=10):
-        self.printAll=True
+    def __init__(self,max_dur=10000):
+        self.isStarted = False
+        self.printAll=False
         self.max_duration = max_dur
         
         self.start_datetime = datetime.now()
         self.script_start_datedir = os.path.join(datadir,self.start_datetime.strftime("%m-%d-%Y"))
         self.script_start_datetime = self.start_datetime.strftime("%m-%d-%Y_%H:%M:%S")
         self.start_timestamp = datetime.timestamp(self.start_datetime)
-
-        os.makedirs(self.script_start_datedir,exist_ok=True)
 
         self.gps_filepath = os.path.join(self.script_start_datedir,"GPS_"+self.script_start_datetime+'.csv')
 
@@ -54,16 +56,12 @@ class DataRecorder:
                         print(self.max_duration,"seconds elapsed")
                         break
                 except KeyError:
-                    print("GPS not returning latitude or longitude")
                     break
                 except StopIteration:
                     session = None
                     print("GPSD has terminated")
                     
     def write_gps_route_raw(self):
-        if len(self.lats)==0 or len(self.longs)==0:
-            print("No GPS coordinates to map on route")
-            return
         # may want to reduce number of coordinates sent in map-query
         map_params = {"key":openstreetmap_apikey,"bestfit":",".join([str(min(self.lats)-.01), str(min(self.longs)-.01), str(max(self.lats)+.01), str(max(self.longs)+.01)]), "size":"1920, 960", "shape":",".join(self.latlongs)}
 
@@ -80,16 +78,13 @@ class DataRecorder:
 
     def button_callback(channel):
         print("Button was pushed, flipping states!")
-
-if __name__=='__main__':
-    dr = DataRecorder()
-    dr.initialize_gps()
-    dr.write_gps_route_raw()
-    print("finished testing suite")
+        if self.isStarted:
+            self.isStarted = False
+        else:
+            self.isStarted = True
 
 # add a callback to external on/off button
 # GPIO.setwarnings(False) # Ignore warning for now
 # GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 # GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 # GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback) # Setup event on pin 10 rising edge
-
